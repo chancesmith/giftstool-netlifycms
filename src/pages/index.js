902de11c33ea370fb24bb5daaf5f5aec9409b592
Chance Smith <chancesmithb@gmail.com>
2018-02-10 03:53:45 -0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+import Script from "react-load-script";
+
+export default class IndexPage extends React.Component {
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+    window.netlifyIdentity.init();
+  }
+
+  render() {
+    const { data } = this.props;
+    const { edges: opportunities } = data.allMarkdownRemark;
+    return (
+      <section className="section">
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
+        <div className="container">
+          <div className="content">
+            <h1 className="has-text-weight-bold is-size-2">Projects</h1>
+          </div>
+          {opportunities
+            .filter(
+              post => post.node.frontmatter.templateKey === "opportunity-post"
+            )
+            .map(({ node: post }) => {
+              return (
+                <div
+                  className="content"
+                  style={{ border: "1px solid #eaecee", padding: "2em 4em" }}
+                  key={post.id}
+                >
+                  <p>
+                    <Link
+                      className="has-text-primary"
+                      to={post.frontmatter.path}
+                    >
+                      {post.frontmatter.title}
+                    </Link>
+                    <span> &bull; </span>
+                    <small>{post.frontmatter.date}</small>
+                  </p>
+                  <p>
+                    {post.excerpt}
+                    <br />
+                    <br />
+                    <Link
+                      className="button is-small"
+                      to={post.frontmatter.path}
+                    >
+                      Keep Reading →
+                    </Link>
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </section>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
